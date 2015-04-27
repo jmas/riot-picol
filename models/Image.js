7507058f80e4247f85d87a55b8f9f15eb6bcd865
jmas <jmas.ukraine@gmail.com>
2015-04-27 22:29:36 +0300

    Clean up.

--- models/Image.js
+++
@@ -1,8 +0,0 @@
-var store = require('../store');
-
-module.exports = store.defineResource({
-  // Why couldn't Mongo just use "id"?
-  idAttribute: '_id',
-  // map this resource to a collection, default is Resource#name
-  table: 'image'
-});
